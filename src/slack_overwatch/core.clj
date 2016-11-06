eea14946d69d11674753e863e93c88b710d753cf
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-11-06 13:23:21 -0700

    Add mild error handling for missing battle.net id

--- src/slack_overwatch/core.clj
+++ src/slack_overwatch/core.clj
@@ -1,7 +1,8 @@
 (ns slack-overwatch.core
   (:require [cheshire.core :refer [parse-string]]
             [clojure.string :as str]
-            [clj-http.client :as client]))
+            [clj-http.client :as client])
+  (:import (clojure.lang ExceptionInfo)))
 
 (def overwatch-api "http://ow-api.herokuapp.com")
 
@@ -12,31 +13,34 @@
 
 (defn profile->attachment
   [data]
-  (let [author-name (:username data)
-        ;author-icon (:avatar data)
-        {:keys [rank rank_img]} (:competitive data)
-        level (:level data)
-        {:keys [wins lost]} (get-in data [:games :competitive])]
-    [{:pretext (str "Competitive ranking for *" author-name "*!")
-      :mrkdwn_in ["pretext"]
-      :author_name author-name
-      ;:author_icon author-icon
-      :thumb_url rank_img
-      :fields [(create-field "Rank" rank true)
-               (create-field "Win/Loss" (str/join "/" [wins lost]) true)]}
-     #_{:fields [(create-field "Level" level true)
-                 (create-field "Win/Loss" (str/join "/" [wins lost]) true)]}
-      :ts (quot (System/currentTimeMillis) 1000)]))
-
+  (if (map? data)
+    (let [author-name (:username data)
+          ;author-icon (:avatar data)
+          {:keys [rank rank_img]} (:competitive data)
+          level (:level data)
+          {:keys [wins lost]} (get-in data [:games :competitive])]
+      [{:pretext     (str "Competitive ranking for *" author-name "*!")
+        :mrkdwn_in   ["pretext"]
+        :author_name author-name
+        ;:author_icon author-icon
+        :thumb_url   rank_img
+        :fields      [(create-field "Rank" rank true)
+                      (create-field "Win/Loss" (str/join "/" [wins lost]) true)]}
+       #_{:fields [(create-field "Level" level true)
+                   (create-field "Win/Loss" (str/join "/" [wins lost]) true)]}
+       :ts (quot (System/currentTimeMillis) 1000)])
+    [{:pretext data
+      :mrkdwn_in ["pretext"]}]))
 
 (defn player-profile [gamer-tag]
   (let [platform "pc"
         region "us"
         tag (str/replace gamer-tag #"#" "-")
         operation "profile"]
-    (parse-string
-      (:body
-        (client/get
-          (str/join "/" [overwatch-api operation platform region tag])))
-      true)))
-
+    (try
+      (parse-string
+        (:body
+          (client/get
+            (str/join "/" [overwatch-api operation platform region tag])))
+        true)
+      (catch ExceptionInfo _ (str "Could not find *" gamer-tag "*. Verify case and/or tag number.")))))
